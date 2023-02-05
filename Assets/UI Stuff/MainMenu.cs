e3dbb7c5c2bd137caabba45e1019679e8812ff58
FelipeZapater <fbzapater@hotmail.com>
2023-02-05 16:58:39 -0300

    Comit

--- Assets/UI Stuff/MainMenu.cs
+++ Assets/UI Stuff/MainMenu.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject options;
+
     void Start()
     {
-        
+        mainMenu.SetActive(true);
+        options.SetActive(false);
     }
-    
-    void Update()
+
+    public void Play()
     {
-        
+        SceneManager.LoadScene("Controles");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
