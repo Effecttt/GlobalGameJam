5668d072a7502658a668ead9a88916c08c3aec40
FelipeZapater <fbzapater@hotmail.com>
2023-02-05 15:37:48 -0300

    UI Start

---
+++ Assets/UI Stuff/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject options;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
